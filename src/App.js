3b829a28cdf98576d79b4bb94f5ad9009ff91284
tania <taniafesun@gmail.com>
2022-01-23 12:56:38 +0200

    hw2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Users from "./components/Users/Users";
+import UserDetails from "./components/UserDetails/UserDetails";
+import Posts from "./components/Posts/Posts";
+import css from './App.module.css'
+
+const App = () => {
+    const [user,setUser] = useState(null);
+    const [userId,setUserId] = useState(null);
+
+    const getUser = (user) => {
+        setUser(user)
+        setUserId(null)
+    }
+    const getUserId = (id) => {
+        setUserId(id)
+    }
+    return (
+        <div>
+            <div className={css.wrap}>
+                <Users getUser = {getUser}/>
+                {user && <UserDetails user={user} getUserId={getUserId}/>}
+            </div>
+            {userId && <Posts userId ={userId}/>}
+        </div>
+    );
+};
 
 export default App;
